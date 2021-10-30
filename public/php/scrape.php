2f26e844330ee4fb6a4a20159209d81f64f7ce6e
Tetsuaki Baba <baba@bagpipe.local>
2021-10-30 14:37:41 +0900

    スクレイピング処理を./php/scrape.phpに集約

--- public/php/scrape.php
+++ public/php/scrape.php
@@ -1,10 +1,14 @@
   
 <?php
-//正規表現については以下のサイトを参考にした
+// 正規表現については以下のサイトを参考にした
 // https://qiita.com/kanaxx/items/daca1c57e48e0a8d674a
+// Scrapeするための引数は GETで取得する
+
+$url = $_GET['url'];
+$regexp = $_GET['regexp'];
 
 //$html = file_get_contents("tmp.html");
-$html = file_get_contents("https://www.sd.tmu.ac.jp/news.html");
+$html = file_get_contents($url);
 
 //改行とタブを全部取っ払い1行にする
 $html = preg_replace('/\r\n|\r|\n|\t/', '', $html);
@@ -14,7 +18,8 @@ $html = preg_replace('/ {2,}/',' ', $html);
 
 
 // 該当するリストタグだけを表示 現状は 2020年以降のみをスクレイピングする
-preg_match_all('@<li class="informationRow age202(?:.*?)>(.*?)</li>@s',$html ,$matches);
+// '@<li class="informationRow age202(?:.*?)>(.*?)</li>@s'
+preg_match_all($regexp, $html ,$matches);
 //print_r($matches);
 foreach( $matches[0] as $match){
     //print_r($match);
