50dfbe98667377f7bdaf057e12c3e12b833211d8
Tetsuaki Baba <baba@bagpipe.local>
2021-10-30 11:32:17 +0900

    ia wikiとの連携機能追加

--- public/get_topics_from_ia_wiki.php
+++ public/get_topics_from_ia_wiki.php
@@ -4,7 +4,7 @@
 // https://qiita.com/kanaxx/items/daca1c57e48e0a8d674a
 
 //$html = file_get_contents("tmp.html");
-$html = file_get_contents("https://www.sd.tmu.ac.jp/news.html");
+$html = file_get_contents("http://industrial-art.sd.tmu.ac.jp/wiki/doku.php?id=website:topics");
 
 //改行とタブを全部取っ払い1行にする
 $html = preg_replace('/\r\n|\r|\n|\t/', '', $html);
@@ -13,8 +13,8 @@ $html = preg_replace('/\r\n|\r|\n|\t/', '', $html);
 $html = preg_replace('/ {2,}/',' ', $html);
 
 
-// 該当するリストタグだけを表示 現状は 2020年以降のみをスクレイピングする
-preg_match_all('@<li class="informationRow age202(?:.*?)>(.*?)</li>@s',$html ,$matches);
+// 該当する箇所だけを選別 
+preg_match_all('@<!-- CONTENT -->(.*?)<!-- \/CONTENT -->@s',$html ,$matches);
 //print_r($matches);
 foreach( $matches[0] as $match){
     //print_r($match);
