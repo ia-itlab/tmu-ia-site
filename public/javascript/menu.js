bec11617bef4bcf2942d6721cdcf75907e53c657
Tetsuaki Baba <baba@bagpipe.local>
2021-11-05 01:19:07 +0900

    interview機能の追加（β版）

--- public/javascript/menu.js
+++ public/javascript/menu.js
@@ -2,8 +2,14 @@ $("#global-nav").load("../parts/menu.html", function () {
     let ul = document.querySelector('#menu');
     let lis = ul.querySelectorAll('li');
     var filename = window.location.href.split('/').pop();
+
+    if (filename.indexOf('labview.html') >= 0) {
+        lis[0].querySelector('a').classList = 'here';
+        return;
+    }
     for (li of lis) {
-        if (li.querySelector('a').id + '.html' === filename) {
+
+        if (li.querySelector('a').id + '.html' == filename) {
             li.querySelector('a').classList = 'here';
         }
         else {
