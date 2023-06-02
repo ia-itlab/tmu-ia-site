bae6ce1754496a6094e1b4cbb8cd57f832171091
TETSUAKI BABA <baba@bolon.local>
2023-06-02 23:00:45 +0900

    fixed #52 wiki上にtopics:archivesのページを別途作成して、そちらに古い情報を移し、もっと見るボタンでアクセスできるように変更

--- public/javascript/topics.js
+++ public/javascript/topics.js
@@ -1,6 +1,100 @@
 var card_data = [];
 
-function generateCards() {
+function generateCard(count_ol, _card, msnry_topics) {
+    let cd = _card;
+    let title = cd.title;
+    let image = cd.image;
+    let abstract = cd.abstract;
+    let date = cd.date;
+    let link = cd.link;
+
+    var col = document.createElement('div');
+    col.style.display = 'block'
+    if (count_ol == 0) {
+        col.classList.add('col', 'col-xs-12', 'col-sm-12', 'col-md-8', 'col-lg-8', 'mt-3', 'g-4', 'grid-item');
+    }
+    else {
+        col.classList.add('col', 'col-xs-12', 'col-sm-12', 'col-md-6', 'col-lg-4', 'mt-3', 'g-4', 'grid-item');
+    }
+
+    // col -> card -> card-body
+    document.getElementById('topics').appendChild(col);
+    let card = document.createElement('div');
+
+    card.classList.add('card');
+    card.style.backgroundColor = 'transparent';
+    card.style.borderColor = 'black';
+
+    //card.innerHTML = 'card';
+    let card_body = document.createElement('div');
+    card_body.classList.add('card-body');
+
+    col.appendChild(card);
+
+    let img_card = document.createElement('img');
+    img_card.classList.add('card-img-top');
+    if (count_ol == 0) {
+        img_card.classList.add('colored');
+    }
+    img_card.style.visibility = 'hidden';
+    let element_loading = createIALoadingElement();
+
+
+    if (cd.image === 'noimage') {
+
+    }
+    else {
+        img_card.onload = function () {
+            element_loading.remove();
+            img_card.style.visibility = 'visible';
+            img_card.classList.add('fadeIn');
+            msnry_topics.layout();
+        }
+        card.appendChild(element_loading);
+        img_card.src = cd.image;
+        card.appendChild(img_card);
+    }
+    card.appendChild(card_body);
+
+    let a_link = document.createElement('a');
+    a_link.href = cd.link;
+    a_link.target = '_blank';
+    a_link.appendChild(card);
+    col.appendChild(a_link);
+
+
+
+    // console.log(lis);
+    let h5_card_title = document.createElement('h5');
+    h5_card_title.classList.add('card-title');
+    h5_card_title.innerHTML = title + ' ';
+    card_body.appendChild(h5_card_title);
+
+    let p_card_text = document.createElement('p');
+    p_card_text.classList.add('card-text');
+    p_card_text.innerHTML = cd.abstract;
+    card_body.appendChild(p_card_text);
+
+    let p_card_text_date = document.createElement('p');
+    p_card_text_date.classList.add('card-text', 'text-muted');
+    p_card_text_date.innerHTML = cd.date;
+    card_body.appendChild(p_card_text_date);
+
+    let i_twitter = document.createElement('i');
+    i_twitter.classList = 'bi bi-twitter';
+
+    let a_twitter = document.createElement('a');
+    a_twitter.href = 'https://twitter.com/intent/tweet?text=' + encodeURI(abstract) + '&url=' + encodeURI(link);
+    a_twitter.target = "_blank";
+    a_twitter.classList = "twitter-link";
+    a_twitter.appendChild(i_twitter);
+    h5_card_title.appendChild(a_twitter);
+
+    msnry_topics.appended(col);
+    count_ol++;
+}
+
+function generateCards(_more) {
     var elem_topics = document.querySelector('#topics');
     var msnry_topics = new Masonry(elem_topics, {
         // options
@@ -11,101 +105,17 @@ function generateCards() {
         originLeft: true,
     });
 
-    var grid_sizer = document.createElement('div');
-    grid_sizer.classList.add('grid-sizer');
-    document.getElementById('topics').appendChild(grid_sizer);
 
+    if (!_more) {
+        var grid_sizer = document.createElement('div');
+        grid_sizer.classList.add('grid-sizer');
+        document.getElementById('topics').appendChild(grid_sizer);
+    }
     let count_ol = 0;
     for (cd of card_data) {
-        let title = cd.title;
-        let image = cd.image;
-        let abstract = cd.abstract;
-        let date = cd.date;
-        let link = cd.link;
-
-        var col = document.createElement('div');
-        col.style.display = 'block'
-        if (count_ol == 0) {
-            col.classList.add('col', 'col-sm-12', 'col-md-8', 'col-lg-8', 'mt-3', 'g-4', 'grid-item');
-        }
-        else {
-            col.classList.add('col', 'col-sm-6', 'col-md-4', 'col-lg-4', 'mt-3', 'g-4', 'grid-item');
-        }
-        // col -> card -> card-body
-        document.getElementById('topics').appendChild(col);
-        let card = document.createElement('div');
-
-        card.classList.add('card');
-
-        //card.innerHTML = 'card';
-        let card_body = document.createElement('div');
-        card_body.classList.add('card-body');
-
-        col.appendChild(card);
-
-        let img_card = document.createElement('img');
-        img_card.classList.add('card-img-top');
-        if (count_ol == 0) {
-            img_card.classList.add('colored');
-        }
-        img_card.style.visibility = 'hidden';
-        let element_loading = createIALoadingElement();
-
-
-        if (cd.image === 'noimage') {
-
-        }
-        else {
-            img_card.onload = function () {
-                element_loading.remove();
-                img_card.style.visibility = 'visible';
-                img_card.classList.add('fadeIn');
-                msnry_topics.layout();
-            }
-            card.appendChild(element_loading);
-            img_card.src = cd.image;
-            card.appendChild(img_card);
-        }
-        card.appendChild(card_body);
-
-        let a_link = document.createElement('a');
-        a_link.href = cd.link;
-        a_link.target = '_blank';
-        a_link.appendChild(card);
-        col.appendChild(a_link);
-
-
-
-        // console.log(lis);
-        let h5_card_title = document.createElement('h5');
-        h5_card_title.classList.add('card-title');
-        h5_card_title.innerHTML = title + ' ';
-        card_body.appendChild(h5_card_title);
-
-        let p_card_text = document.createElement('p');
-        p_card_text.classList.add('card-text');
-        p_card_text.innerHTML = cd.abstract;
-        card_body.appendChild(p_card_text);
-
-        let p_card_text_date = document.createElement('p');
-        p_card_text_date.classList.add('card-text', 'text-muted');
-        p_card_text_date.innerHTML = cd.date;
-        card_body.appendChild(p_card_text_date);
-
-        let i_twitter = document.createElement('i');
-        i_twitter.classList = 'bi bi-twitter';
-
-        let a_twitter = document.createElement('a');
-        a_twitter.href = 'https://twitter.com/intent/tweet?text=' + encodeURI(abstract) + '&url=' + encodeURI(link);
-        a_twitter.target = "_blank";
-        a_twitter.classList = "twitter-link";
-        a_twitter.appendChild(i_twitter);
-        h5_card_title.appendChild(a_twitter);
-
-        msnry_topics.appended(col);
+        generateCard(count_ol, cd, msnry_topics);
         count_ol++;
     }
-    //console.log('topics', document.getElementById('topics'));
 
 }
 
@@ -137,6 +147,7 @@ window.onload = function () {
                 xmlhttp.send(null);
             }
         });
+
         var promise_sd_news = new Promise(function (resolve, reject) {
 
             // 大学ウェブサイトからニュースを取得する
@@ -167,6 +178,7 @@ window.onload = function () {
 
         });
 
+
         await promise_topics;
         {
             var ols = document.querySelectorAll('ol.fix-media-list-overlap');
@@ -182,8 +194,10 @@ window.onload = function () {
                 });
             }
             document.querySelector('.dw-content').remove();
-
         }
+
+
+
         await promise_sd_news;
         {
             var lists = document.getElementById('topics').querySelectorAll('li');
@@ -204,14 +218,70 @@ window.onload = function () {
             }
 
         }
+
+
         // ここまでで、card_dataに必要な情報がはいる
         card_data = card_data.sort(function (a, b) {
             return (a.date > b.date) ? -1 : 1;
         });
-        //console.log(card_data);
 
         loadFont();
-        generateCards();
+        generateCards(false);
+
+        // button要素を #topics に追加する
+        let button = document.createElement('button');
+        button.classList = 'btn btn-outline-dark btn-block mt-4';
+        button.innerHTML = 'もっと見る';
+        document.getElementById('contents').appendChild(button);
+        button.onclick = async function () {
+            var promise_topics_archives = new Promise(function (resolve, reject) {
+                // ia wiki から topicsデータを取得する
+                {
+                    var url = 'http://industrial-art.sd.tmu.ac.jp/wiki/doku.php\?id=website:topics:archives';
+                    var regexp = '@<!-- CONTENT -->(.*?)<!-- \/CONTENT -->@s';
+                    var _html = './php/scrape.php?regexp=' + regexp + '&url=' + url;
+                    var replace = 'topics';
+                    //Httpリクエスを作る
+                    var xmlhttp = new XMLHttpRequest();
+                    xmlhttp.open("GET", _html, true);
+                    xmlhttp.onreadystatechange = function () {
+                        //とれた場合Idにそって入れ替え
+                        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                            var data = xmlhttp.responseText;
+                            var elem = document.getElementById(replace);
+                            // わざわざdomに入れるのは、querySelector等で検索処理しやすいから
+                            elem.innerHTML += data;
+
+                            resolve(data);
+                        }
+                    }
+                    xmlhttp.send(null);
+                }
+            });
+
+
+            card_data = [];
+            await promise_topics_archives;
+            {
+                var ols = document.querySelectorAll('ol.fix-media-list-overlap');
+                for (ol of ols) {
+                    let lis = ol.querySelectorAll('li');
+
+                    card_data.push({
+                        title: lis[0].querySelector('div').innerHTML.trimStart(),
+                        image: lis[1].querySelector('div').querySelector('a').href,
+                        abstract: lis[2].querySelector('div').innerHTML.trimStart(),
+                        date: lis[3].querySelector('div').innerHTML.trimStart(),
+                        link: lis[4].querySelector('div').querySelector('a').href
+                    });
+                }
+                document.querySelector('.dw-content').remove();
+
+            }
+            //document.querySelector('#topics').innerHTML = '';
+            generateCards(true);
+            button.remove();
+        }
     }
 
 
